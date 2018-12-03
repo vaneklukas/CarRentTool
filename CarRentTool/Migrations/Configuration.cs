1899e27f0f4fad904dcd63195358691d1fcc417d
Lukáš Vaněk <vanek.lukas.lvk@gmail.com>
2018-12-03 19:17:23 +0100

    init version

---
+++ CarRentTool/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace CarRentTool.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CarRentTool.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CarRentTool.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ CarRentTool/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarRentTool.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Km { get; set; }
+        public int NoSeats { get; set; }
+
+    }
+}
